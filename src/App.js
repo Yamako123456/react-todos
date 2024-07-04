5a741435bd5b410bea2d856c8b830b020aac2657
yamako <yamakodev@gmail.com>
2024-07-03 21:13:14 -0700

    delete todo from todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import logo from './logo.svg';
+
+import React, {useState} from 'react';
 import './App.css';
+import NewTodoForm from './components/NewTodoForm';
+
+import TodoTable from './components/TodoTable';
+
 
 function App() {
+  
+  const [todos, setTodos] = useState([
+    {
+      rowNumber: 1,
+      rowDescription: 'Feed dog',
+      rowAssigned: 'Eric'
+    },
+    {
+      rowNumber: 2,
+      rowDescription: 'Charge phone',
+      rowAssigned: 'Eric'
+    },
+    {
+      rowNumber: 3,
+      rowDescription: 'Get haircut',
+      rowAssigned: 'Eric'
+    },
+    {
+      rowNumber: 4,
+      rowDescription: 'Find Miquella',
+      rowAssigned: 'Marika'
+    },
+  ]
+  )
+
+  const addTodo = (description, assigned) => {
+    let rowNum = 0;
+    if (todos.length > 0) {
+      rowNum = todos[todos.length - 1].rowNumber + 1
+    } else
+      rowNum = 1;
+
+    const newTodo= {
+      rowNumber: rowNum,  
+      rowDescription: description,
+      rowAssigned: assigned
+    }
+    setTodos(todos => [...todos, newTodo]); 
+  }
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter( function (value) {
+      return value.rowNumber !== deleteTodoRowNumber
+    });
+    setTodos(filtered); 
+  }
+
+
   return (
-    <div>
-      <div>
-        <div>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
           Your Todo's
         </div>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th scope='col'>#</th>
-                <th scope='col'></th>
-                <th scope='col'></th>
-              </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-          </table>
+        <div className='card-body'>
+          <TodoTable todos={todos}  deleteTodo={deleteTodo}/>
+          <button className='btn btn-primary' >
+          Add new tod'
+          </button>
+          <NewTodoForm addTodo={addTodo}/>
         </div>
       </div>
     </div>
