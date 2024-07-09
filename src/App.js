e865babf7231061b0c9033ae16a46055c14ad403
yamako <yamakodev@gmail.com>
2024-07-09 15:56:04 -0700

    Conditional rendering added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import TodoTable from './components/TodoTable';
 
 
 function App() {
-  
+
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
   const [todos, setTodos] = useState([
     {
       rowNumber: 1,
@@ -63,10 +65,12 @@ function App() {
         </div>
         <div className='card-body'>
           <TodoTable todos={todos}  deleteTodo={deleteTodo}/>
-          <button className='btn btn-primary' >
-          Add new tod'
+          <button onClick={() => setShowAddTodoForm(!showAddTodoForm)} className='btn btn-primary' >
+            {showAddTodoForm ? 'Close New Todo' : 'New Todo'}
           </button>
-          <NewTodoForm addTodo={addTodo}/>
+          {showAddTodoForm && 
+            <NewTodoForm addTodo={addTodo}/>
+          }
         </div>
       </div>
     </div>
